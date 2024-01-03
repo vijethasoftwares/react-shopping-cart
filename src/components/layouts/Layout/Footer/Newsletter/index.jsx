9406ca8267fda8b75edeb491e08ce56a592190bb
jayandra06 <68438965+jayandra06@users.noreply.github.com>
2024-01-04 00:29:44 +0530

    init

--- src/components/layouts/Layout/Footer/Newsletter/index.jsx
+++ src/components/layouts/Layout/Footer/Newsletter/index.jsx
@@ -40,7 +40,7 @@ const Newsletter = () => {
     <>
       <section className={styles.section}>
         <div className={styles.container}>
-          <h3 className={styles.title}>Sign up for the FLAAKKO newsletter</h3>
+          <h3 className={styles.title}>Sign up for the ROFYS newsletter</h3>
           <form
             className={styles.form}
             onSubmit={handleSubmit}
